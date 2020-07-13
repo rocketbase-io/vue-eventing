642ec6a71b291c9cf448da98436149932fdc1e77
Merlin Reichwald <merlin.reichwald@rocketbase.io>
2020-07-13 16:37:51 +0200

    feat: export decorator, types and bus

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { config, VueEventingOptions } from "src/config";
 export * from "src/mixin";
+export * from "src/decorator";
+export * from "src/bus";
+export * from "src/types";
 
 export function VueEventing({ instanceMethods, emitIntegration }: VueEventingOptions = {}): void {
   if (instanceMethods) config.instanceMethods = true;
